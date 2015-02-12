9b3578671351d9252f4cb5591723aab812ed4a6f
Cody Burrows <code1010@rochester.rr.com>
2015-02-12 07:59:36 -0500

    Finished constructor....
    Don't judge me if it doesn't compile...I wasn't using an IDE

--- src/USSEnterprise.java
+++ src/USSEnterprise.java
@@ -17,9 +17,22 @@ public class USSEnterprise extends Ship{
 	private boolean hullCompromised;
 	
 	
-	public USSEnterprise(String n, int h, int g, int s, int p, int pow) {
+	public USSEnterprise(String n, int h, int g, int s, int p, int pow, String capt) {
 		super(n, h, g, s, p, pow);
-		
+		shieldsUp = true;
+		destination = "none";
+		warpFactor = 0;
+		velocity = 0;
+		eta = 0;
+		shieldLevel = 100;
+		photons = 64;
+		weaponsUp = false;
+		captain = capt;
+		phaserLevel = 0;
+		computerFailure = false;
+		warpDriveFailure = false;
+		weaponsFailure = false;
+		hullCompromised = false;
 	}
 
 }
